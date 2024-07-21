212d9b17435e08b60f32c5f6f5df6bd3f2b7bbe3
knford01 <54276167+knford01@users.noreply.github.com>
2024-07-21 15:37:53 -0500

    Theme error checking

--- components/theme/theme.d.ts
+++ components/theme/theme.d.ts
@@ -1,12 +1,1 @@
-declare module '@mui/material/styles' {
-    interface Components extends Record<string, any> {
-        MuiDataGrid?: {
-            styleOverrides?: {
-                root?: any;
-                columnHeaders?: any;
-                cell?: any;
-                // Add other DataGrid parts if needed
-            };
-        };
-    }
-} 
+
